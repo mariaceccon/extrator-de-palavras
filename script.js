1f5cf9c8b633c5d749191545468d1b1d3146c02c
mariaceccon <166859052+mariaceccon@users.noreply.github.com>
2025-09-05 17:07:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChaves);
+function mostraPalavrasChaves(){
+    alert("fui clicado!");
+}
+
